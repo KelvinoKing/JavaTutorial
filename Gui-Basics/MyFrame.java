f7c967c88adb766380580ea38655d831b7bbbce8
KelvinoKing <kelvinoking@outlook.com>
2023-12-05 19:42:36 +0300

    Added a component in my Frame

--- Gui-Basics/MyFrame.java
+++ Gui-Basics/MyFrame.java
@@ -1,8 +1,13 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class MyFrame{
     public static void main(String[] args){
-        JFrame myFrame = new JFrame();
+        JFrame myFrame = new JFrame("MyFrameWithComponents");
+
+        JButton btn = new JButton("Ok");
+        myFrame.add(btn);
+
         myFrame.setSize(200,200);
         myFrame.setLocationRelativeTo(null);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
