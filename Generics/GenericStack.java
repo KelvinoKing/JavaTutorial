8cef3d6f8e365a66012c9a83a7c2bde5331b610a
KelvinoKing <kelvinoking@outlook.com>
2023-12-22 15:49:54 +0300

    Generic Stack using array

---
+++ Generics/GenericStack.java
@@ -0,0 +1,50 @@
+public class GenericStack<E> {
+   private static final int DEFAULTCAPACITY = 10;
+   private Object[] list; 
+   private int size = 0;
+
+   public GenericStack(){
+    list = new Object[DEFAULTCAPACITY];
+    size = 0;
+   }
+
+    public int getSize(){
+        return size;
+    }
+    public E peek(){
+        if (isEmpty()){
+            throw new java.util.EmptyStackException();
+        }
+
+        return (E) list[size - 1];
+    }
+    public void push(E o){
+        ensureCapacity();
+        list[size++] = o;
+    }
+    public E pop(){
+        if (isEmpty()){
+            throw new java.util.EmptyStackException();
+        }
+        E o = (E) list[--size];
+        list[size] = null;
+        return o;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public void ensureCapacity(){
+        if (size == list.length){
+            int newCapacity = list.length * 2;
+            list = java.util.Arrays.copyOf(list, newCapacity);
+        }
+    }
+    @Override
+    public String toString(){
+        StringBuilder result = new StringBuilder("Stack: ");
+        for (int i = 0; i <  size; i++){
+            result.append(list[i]).append(" ");
+        }
+        return result.toString();
+    }
+}
